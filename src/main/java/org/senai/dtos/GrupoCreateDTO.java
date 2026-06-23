fbef6e639fd202fd9b9f30b8d8de688568246fd1
augusto_tavares <augusto.saravia.tavares@gmail.com>
2026-06-23 19:58:21 -0300

    GrupoCreateDTO e alteração no pom

---
+++ src/main/java/org/senai/dtos/GrupoCreateDTO.java
@@ -0,0 +1,21 @@
+package org.senai.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record GrupoCreateDTO(
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(max = 100, message = "O nome não pode ter mais de 100 caracteres")
+        String nome,
+
+        List<String> tokens,
+
+        List<Long> supervisorIds,
+
+        LocalDate dataAdmissaoInicio,
+
+        LocalDate dataAdmissaoFim
+) {}
